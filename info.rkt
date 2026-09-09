bd98749df35fc3402e1708430769f2b3570fd5bd
Jos Koot <jos.koot@gmail.com>
2026-09-09 10:58:14 +0200

    Refactor info.rkt to update language and collection name

--- info.rkt
+++ info.rkt
@@ -1,8 +1,7 @@
-
 #lang info
 
 ; By Jacob J. A. Koot
 
 (define scribblings '(("GUI.scrbl" ())))
-; (define collection "GUI-tower-of-hanoi")
+(define collection "GUI")
 (define name "tower-of-hanoi")
